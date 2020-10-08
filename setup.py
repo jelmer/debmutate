ba690dd8e69abd1857286fa3ec5b52709728bc43
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-10-08 17:47:44 +0000

    Update one more version number.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name="debmutate",
-    version="0.1",
+    version="0.10",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["debmutate", "debmutate.tests"],
