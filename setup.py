f523eff8d10226b20a2047d25f57ac1580bb36bf
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-14 22:58:43 +0000

    Bump.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name="debmutate",
-    version="0.25",
+    version="0.26",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["debmutate", "debmutate.tests"],
