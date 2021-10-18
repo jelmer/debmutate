050523cf4a20141f5513aae027458a6b9cf3b9fe
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-10-18 18:16:21 +0100

    Cope with git snapshots.

--- debmutate/deb822.py
+++ debmutate/deb822.py
@@ -34,7 +34,7 @@ from typing import Iterable, List, Optional
 
 try:
     from debian import __version__ as python_debian_version
-    if tuple([int(x) for x in python_debian_version.split('.')]) < (0, 1, 42):
+    if tuple([int(x.split('+')[0]) for x in python_debian_version.split('.')]) < (0, 1, 42):
         # Avoid older versions since there are known bad issues.
         raise ModuleNotFoundError
     from debian._deb822_repro.parsing import (
