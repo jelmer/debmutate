c89339ccb1e6e94a169f142f2bd385fe41bf0eac
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-10-23 18:57:53 +0100

    bump version.

--- debmutate/__init__.py
+++ debmutate/__init__.py
@@ -2,5 +2,5 @@
 
 """Make changes to Debian packages."""
 
-__version__ = (0, 45)
+__version__ = (0, 46)
 version_string = '.'.join([str(x) for x in __version__])
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name="debmutate",
-    version="0.45",
+    version="0.46",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["debmutate", "debmutate.tests"],
